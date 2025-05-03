d5e787146aaea0ca4bdd8e9f9e11d824d0db4864
Pablo Siegrist <psiegristm@outlook.com>
2025-05-03 21:30:34 +0200

    first push

---
+++ CatalogService/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/books", () => new[]
+{
+    new { Id = 1, Title = "Clean Code", Author = "Robert C. Martin" },
+    new { Id = 2, Title = "The Pragmatic Programmer", Author = "Andrew Hunt" },
+    new { Id = 3, Title = "Domain-Driven Design", Author = "Eric Evans" }
+});
+
+app.Run();
